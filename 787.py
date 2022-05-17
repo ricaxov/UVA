6d9d978fbb01b32841aeb2e335198bc231dc86d1
ricaxov <103327245+ricaxov@users.noreply.github.com>
2022-05-17 18:20:07 -0300

    Update 787.py

--- 787.py
+++ 787.py
@@ -3,9 +3,9 @@ while True:
         x=list(map(int, input().split()))
         res=-1000000
         for i in range(len(x)-1):
-            ans=int(x[i])
+            ans=x[i]
             for j in range(i+1, len(x)-1):
-                ans*=int(x[j])
+                ans*=x[j]
                 res=max(res, ans)
         print(max(res,ans))
     except EOFError:
