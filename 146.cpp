c1bf0b149caa70a76bc9e716931f2dabc0d1e093
ricaxov <103327245+ricaxov@users.noreply.github.com>
2022-05-23 10:54:35 -0300

    Create 146.cpp

---
+++ 146.cpp
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h>
+#define f first
+#define s second
+#define pi 3.14159
+#define pb push_back
+#define tos to_string
+#define inf 1000000000
+#define inf64 1000000000000000000
+#define entrada freopen("input.txt", "r", stdin);
+#define saida freopen("output.txt", "w", stdout);
+
+using namespace std;
+
+typedef long long ll;
+typedef pair<int,int> ii;
+
+int main(){
+    ios_base::sync_with_stdio(false);cin.tie(NULL);
+    string str; cin>>str;
+    while(str!="#"){
+        if(next_permutation(str.begin(), str.end())){
+            cout<<str<<endl;
+        }
+        else{
+            cout<<"No Successor"<<endl;
+        }
+        cin>>str;
+    }
+}
