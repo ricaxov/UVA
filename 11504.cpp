85d822d41b7e73d4faa18b4217263e2cb1008686
ricaxov <103327245+ricaxov@users.noreply.github.com>
2022-05-23 12:47:45 -0300

    Create 11504.cpp

---
+++ 11504.cpp
@@ -0,0 +1,69 @@
+#include <bits/stdc++.h>
+#define f first
+#define s second
+#define pi 3.14159
+#define pb push_back
+#define tos to_string
+#define inf 1000000000
+#define inf64 1000000000000000000
+#define entrada freopen("input.txt", "r", stdin);
+#define saida freopen("output.txt", "w", stdout);
+
+using namespace std;
+
+typedef long long ll;
+typedef pair<int,int> ii;
+
+stack<int>stk;
+vector<int>adj[100001];
+vector<bool>vis(100001);
+void dfs1(int v){
+    vis[v]=true;
+    for(int i : adj[v]){
+        if(!vis[i]){
+            dfs1(i);
+        }
+    }
+    stk.push(v);
+}
+void dfs2(int v){
+    vis[v]=true;
+    for(int i : adj[v]){
+        if(!vis[i]){
+            dfs2(i);
+        }
+    }
+}
+int main(){
+    ios_base::sync_with_stdio(false);cin.tie(NULL);
+    int t,n,e,x,y; cin>>t;
+    while(t--){
+        int cont=0;
+        cin>>n>>e;
+        for(int i=0; i<e; i++){
+            cin>>x>>y;
+            x--; y--;
+            adj[x].pb(y);
+        }
+        for(int i=0; i<n; i++){
+            if(!vis[i]){
+                dfs1(i);
+            }
+        }
+        for(int i=0; i<100001; i++){
+            vis[i]=false;
+        }
+        while(!stk.empty()){
+            int u=stk.top(); stk.pop();
+            if(!vis[u]){
+                cont++;
+                dfs2(u);
+            }
+        }
+        cout<<cont<<endl;
+        for(int i=0; i<100001; i++){
+            adj[i].clear();
+            vis[i]=false;
+        }
+    }
+}
