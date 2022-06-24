39028bcfd0e1bce93947e49c69f03e22f4fe5377
ricaxov <103327245+ricaxov@users.noreply.github.com>
2022-06-24 08:13:42 -0300

    Update 11463.cpp

--- 11463.cpp
+++ 11463.cpp
@@ -19,9 +19,7 @@ void floydWarshall(int n){
     for(int k=0; k<n; k++){
         for(int i=0; i<n; i++){
             for(int j=0; j<n; j++){
-                if(dist[i][j]>dist[i][k]+dist[k][j]){
-                    dist[i][j]=min(dist[i][j], dist[i][k]+dist[k][j]);
-                }
+                dist[i][j]=min(dist[i][j], dist[i][k]+dist[k][j]);
             }
         }
     }
